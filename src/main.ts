bd487efa824f0085c83ae92b6bc638a89769d329
sk-chan <chanchanakoedtho@gmail.com>
2023-07-06 16:35:27 +0700

    Fixed index

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,53 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
+import { Octokit, App } from "octokit";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // const ms: string = core.getInput('milliseconds')
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    // core.setOutput('time', new Date().toTimeString())
+
+    const token:string = core.getInput('auth');
+    const repo:string = core.getInput('repo');
+    const varName:string = core.getInput('name');
+    const value:string = core.getInput('value');
+    const owner:string = core.getInput('owner');
+    const path:string = ' https://api.github.com';
+
+
+     // Octokit.js
+    // https://github.com/octokit/core.js#readme
+    const octokit = new Octokit({
+      auth: token
+    })
+
+    const response = await octokit.request(`${path}/repos/${owner}/${repo}/actions/variables/${varName}`, {
+      owner: owner,
+      repo: repo,
+      name: varName,
+      value: value,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+
+
+
+    // Access the desired information from the response object
+    const { status, data } = response;
+    core.info(`Status: ${status}`);
+    core.info(`Response data: ${JSON.stringify(data)}`);
+
+    core.setOutput('result',  status == 204 );
+
+
+
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
