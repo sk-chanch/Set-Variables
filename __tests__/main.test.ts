89d3094b0dc8c7aabca52fe8c83b41632cd37259
sk-chan <chanchanakoedtho@gmail.com>
2023-07-20 15:38:48 +0700

    Added @octokit/rest

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,5 @@
 import {wait} from '../src/wait'
 import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
 
@@ -10,6 +8,6 @@ import {expect, test} from '@jest/globals'
 test('test action', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
 
-  
+
 
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { Octokit } from 'octokit'
+import { Octokit } from '@octokit/rest'
 
 
 
