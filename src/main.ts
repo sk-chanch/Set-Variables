9702a8099d1d240574d8135a6674dd066a7fe699
sk-chan <chanchanakoedtho@gmail.com>
2023-07-20 14:53:22 +0700

    Fixed position Octokit init

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,30 @@
 import * as core from '@actions/core'
 import { Octokit } from 'octokit'
 
+
+
+const token: string = core.getInput('auth')
+const repo: string = core.getInput('repo')
+const varName: string = core.getInput('name')
+const value: string = core.getInput('value')
+const owner: string = core.getInput('owner')
+   
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+const octokit = new Octokit({
+  auth: token
+})
+
+
 async function run(): Promise<void> {
   try {
 
-    const token: string = core.getInput('auth')
-    const repo: string = core.getInput('repo')
-    const varName: string = core.getInput('name')
-    const value: string = core.getInput('value')
-    const owner: string = core.getInput('owner')
-   
-    // Octokit.js
-    // https://github.com/octokit/core.js#readme
-    const octokit = new Octokit({
-      auth: token
-    })
-
+    
     const response = await octokit.rest.actions.createRepoVariable({
+      name: varName,
       owner: owner,
       repo: repo,
-      name: varName,
-      value: value,
+      value: value
     })
 
   
